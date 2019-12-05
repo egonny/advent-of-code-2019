614bbc9d83ed3163c04288f21df810455782f880
Egon Okerman <dev@egon.me>
2019-12-06 00:42:56 +0100

    Finish day 1 solution

---
+++ src/day1.py
@@ -0,0 +1,28 @@
+import math
+
+INPUT = "input/1.in"
+
+def read_input():
+    with open(INPUT) as f:
+        content = f.readlines()
+        return list(map(lambda x: int(x), content))
+
+def calc_fuel(mass):
+    return math.floor(mass / 3) - 2
+
+# PART 1
+input = read_input()
+total_fuel = sum(map(lambda x: calc_fuel(x), input))
+print(f"Total fuel requirements: {total_fuel}")
+
+# PART 2
+def calc_fuel_repeated(mass):
+    total = 0
+    fuel = calc_fuel(mass)
+    while fuel > 0:
+        total += fuel
+        fuel = calc_fuel(fuel)
+    return total
+
+total_fuel_repeated = sum(map(lambda x: calc_fuel_repeated(x), input))
+print(f"Total fuel repeated requirements: {total_fuel_repeated}")
