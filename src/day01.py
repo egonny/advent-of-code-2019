79e326529092d3d1d06352b5e386f04edf34b34e
egonny <dev@egon.me>
2019-12-07 17:15:49 +0100

    Py optimizations thanks to Darragh Van Tichelen

--- src/day01.py
+++ src/day01.py
@@ -4,15 +4,14 @@ INPUT = "input/01.in"
 
 def read_input():
     with open(INPUT) as f:
-        content = f.readlines()
-        return list(map(lambda x: int(x), content))
+        return [int(x) for x in f]
 
 def calc_fuel(mass):
     return math.floor(mass / 3) - 2
 
 # PART 1
 input = read_input()
-total_fuel = sum(map(lambda x: calc_fuel(x), input))
+total_fuel = sum([calc_fuel(x) for x in input])
 print(f"Total fuel requirements: {total_fuel}")
 
 # PART 2
@@ -24,5 +23,5 @@ def calc_fuel_repeated(mass):
         fuel = calc_fuel(fuel)
     return total
 
-total_fuel_repeated = sum(map(lambda x: calc_fuel_repeated(x), input))
+total_fuel_repeated = sum([calc_fuel_repeated(x) for x in input])
 print(f"Total fuel repeated requirements: {total_fuel_repeated}")
